fc099b891683d4b0fc15ed1c8e3c7277734a9874
Nevaja Ingraham <nsi3420@ppsbstudent.org>
2018-11-05 22:13:26 +0000

    recover files

---
+++ projects/platformer/js/init/collectable.js
@@ -0,0 +1,49 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let collectable = window.opspark.collectable;
+    
+    let type = {
+        db: {assetKey: 'db', points: 10},
+        max: {assetKey: 'max', points: 20},
+        steve: {assetKey: 'steve', points: 30},
+        grace: {assetKey: 'grace', points: 40},
+        kennedi: {assetKey: 'kennedi', points: 50}
+    };
+    
+    /**
+     * init: Initialize all collectables.
+     * 
+     * GOAL: Add as many collectables as necessary to make your level challenging.
+     * 
+     * Use the collectable.create() method to create collectables for the level.
+     * See the type Object, above, for the types of collectables and their point values.
+     * 
+     * collectable.create() takes these arguments:
+     *      
+     *      collectable.create(type, x, y, gravity, bounce);
+     * 
+     *      type: The type of the collectable, use the type Object above.
+     *      x: The x coordineate for the collectable.
+     *      y: The y coordineate for the collectable.
+     *      gravity: OPTIONAL The gravitational pull on the collectable.
+     *      bounce: OPTIONAL A factor effecting how much the collectable will bounce off platforms, etc.
+     */ 
+    collectable.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        collectable.create(type.max, 300, 170, 10, .2);
+        collectable.create(type.steve, 50, 70, 10, .2);
+        collectable.create(type.grace, 600, 120, 10, .2 );
+        collectable.create(type.kennedi, 215, 81, 10, .2);
+        
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
